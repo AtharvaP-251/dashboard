b011b52b21974989690bae2225df04155b5a56fe
AtharvaP-251 <patharva266@gmail.com>
2024-05-06 00:12:52 +0530

    Fix warning: A props object containing a key prop is being spread into JSX

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import { Chip } from "@mui/material";
 
 export default function Tags({ options, label, width, onChange }) {
     const handleChange = (event, options) => {
@@ -16,6 +17,22 @@ export default function Tags({ options, label, width, onChange }) {
                 options={options}
                 getOptionLabel={(option) => option.label}
                 filterSelectedOptions
+                renderOption={(props, option) => {
+                    return (
+                        <li {...props} key={option.label}>
+                            {option.label}
+                        </li>
+                    );
+                }}
+                renderTags={(tagValue, getTagProps) => {
+                    return tagValue.map((option, index) => (
+                        <Chip
+                            {...getTagProps({ index })}
+                            key={option.label}
+                            label={option.label}
+                        />
+                    ));
+                }}
                 renderInput={(params) => (
                     <TextField {...params} label={label} />
                 )}
